f92181c7272fed1a262961ffa0ed0d3a6fec6821
latin1gor <latinigor.main@gmail.com>
2024-08-21 16:12:09 +0300

    added adaptive helpers

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import {Text} from "react-native";
+
+
+const HomeScreen = () => {
+
+    return (
+        <Text>
+            Hello brouudie
+        </Text>
+    )
+}
+
+export default HomeScreen
