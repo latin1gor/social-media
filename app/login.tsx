da9445da324b99a702b0fbe8d9579682c0fb5284
latin1gor <latinigor.main@gmail.com>
2024-08-27 14:49:15 +0300

    added home icon

--- app/login.tsx
+++ app/login.tsx
@@ -1,11 +1,14 @@
 import {Text} from "react-native";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import Home from "@/assets/icons/home";
+import {theme} from "@/constants/theme";
 
 
 const Login = () => {
     return (
             <ScreenWrapper>
                 <Text>Login page </Text>
+                <Home strokeWidth={2} color={theme.colors.primary} />
             </ScreenWrapper>
     )
 }
