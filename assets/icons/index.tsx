c361f16ae66d7ce213fe54b50cc4dc6f21e8cf3e
latin1gor <latinigor.main@gmail.com>
2024-08-28 20:39:25 +0300

    fix ts error

--- assets/icons/index.tsx
+++ assets/icons/index.tsx
@@ -1,12 +1,28 @@
 import {View} from "react-native";
+import Home from "@/assets/icons/home";
+import React, {ReactNode} from "react";
+import {SvgProps} from "react-native-svg";
+import {theme} from "@/constants/theme";
 
+interface IIcons {
+    home: (props: SvgProps) => React.JSX.Element
+}
+interface IProps {
+    name: keyof IIcons
+    props: SvgProps
+}
 
-const Icon = ({}) => {
+const icons: IIcons = {
+    home: Home
+}
+
+const Icon = ({name, ...props}: IProps) => {
+    const IconComponent = icons[name]
     return (
-        <View>
+        <IconComponent height={props.size || 24} width={props.size || 24} strokeWidth={props.strokeWidth || 1.9} color={theme.colors.textLight} {...props}  />
 
 
-        </View>
+
     )
 }
 
