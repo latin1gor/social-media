5e6f2852fdf4dafa80cd97ab03cd4e0c6d396974
latin1gor <latinigor.main@gmail.com>
2024-09-06 15:56:01 +0300

    linked constants

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,8 @@ import { AppState } from 'react-native'
 import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import {supabaseAnonKey, supabaseUrl} from "@/constants";
 
-const supabaseUrl = YOUR_REACT_NATIVE_SUPABASE_URL
-const supabaseAnonKey = YOUR_REACT_NATIVE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
