2cdccecb98d308a9dbef1ea70e320d599bf1fe00
latin1gor <latinigor.main@gmail.com>
2024-09-10 16:00:13 +0300

    logout button setup

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -1,10 +1,19 @@
-import {Text, View} from "react-native";
+import {Button, Text, View} from "react-native";
 import ScreenWrapper from "@/components/screen-wrapper";
+import {useAuth} from "@/hooks/useAuth";
 
 const Home = () => {
+
+    const { setAuth } = useAuth();
+
+    const onLogout = async () => {
+
+    }
+
     return (
         <ScreenWrapper>
             <Text>Home</Text>
+            <Button title={"Log out"} onPress={onLogout} />
         </ScreenWrapper>
     );
 };
