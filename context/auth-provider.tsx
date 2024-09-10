e6b684e3d10add9c8a015dd46c9386f87c3ceb21
latin1gor <latinigor.main@gmail.com>
2024-09-10 14:30:30 +0300

    added authProvider

---
+++ context/auth-provider.tsx
@@ -0,0 +1,20 @@
+import {createContext, PropsWithChildren, useState} from "react";
+import {User} from "@supabase/auth-js";
+
+
+const AuthContext = createContext({})
+
+const AuthProvider = ({children}: PropsWithChildren) => {
+    const [user, setUser] = useState<User | null>(null)
+
+    const setAuth = (authUser: User) => setUser(authUser)
+
+    const setUserData = (data: any) => setUser(prevUser => ({...prevUser, ...data}))
+
+    return (
+        <AuthContext.Provider value={{user, setAuth, setUserData}}>{children}</AuthContext.Provider>
+    )
+
+}
+
+export default AuthProvider
