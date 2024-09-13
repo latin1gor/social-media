5c80ca9cd471fc88684d8ea7d15a3533268694fe
latin1gor <latinigor.main@gmail.com>
2024-09-13 13:38:27 +0300

    user profile page setup

--- app/(main)/profile.tsx
+++ app/(main)/profile.tsx
@@ -1,11 +1,25 @@
-import {View} from "react-native";
+import { Text, View } from "react-native";
+import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "expo-router";
+import ScreenWrapper from "@/components/screen-wrapper";
 
 const Profile = () => {
-    return (
-        <View>
+  const { user, setAuth } = useAuth();
+  const router = useRouter();
+  return (
+    <ScreenWrapper bg={"white"}>
+      <UserHeader user={user} router={router} />
+      <Text>Profile</Text>
+    </ScreenWrapper>
+  );
+};
 
-        </View>
-    );
+const UserHeader = ({ user, router }) => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Text>User header</Text>
+    </View>
+  );
 };
 
 export default Profile;
