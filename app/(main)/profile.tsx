8c251ce6f6d59c6e5ba594d7b99dc2a63eb35b12
latin1gor <latinigor.main@gmail.com>
2024-09-13 14:19:35 +0300

    logout button

--- app/(main)/profile.tsx
+++ app/(main)/profile.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "expo-router";
 import ScreenWrapper from "@/components/screen-wrapper";
@@ -8,6 +8,7 @@ import { wp } from "@/helpers/common";
 const Profile = () => {
   const { user, setAuth } = useAuth();
   const router = useRouter();
+
   return (
     <ScreenWrapper bg={"white"}>
       <UserHeader user={user} router={router} />
@@ -17,15 +18,25 @@ const Profile = () => {
 };
 
 const UserHeader = ({ user, router }) => {
+  const handleLogout = async () => {};
+
   return (
     <View
       style={{ flex: 1, backgroundColor: "white", paddingHorizontal: wp(4) }}
     >
       <View>
         <Header title={"Profile"} showBackButton />
+        <TouchableOpacity
+          style={styles.logoutButton}
+          onPress={handleLogout}
+        ></TouchableOpacity>
       </View>
     </View>
   );
 };
 
 export default Profile;
+
+const styles = StyleSheet.create({
+  logoutButton: {},
+});
