6596a361a0f60ea73524f7bbc3780f75767eae45
latin1gor <latinigor.main@gmail.com>
2024-09-13 14:44:35 +0300

    logout modal + functionality

--- app/(main)/profile.tsx
+++ app/(main)/profile.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Alert, StyleSheet, TouchableOpacity, View } from "react-native";
 import { useAuth } from "@/hooks/useAuth";
 import { Router, useRouter } from "expo-router";
 import ScreenWrapper from "@/components/screen-wrapper";
@@ -6,37 +6,59 @@ import Header from "@/components/Header";
 import { hp, wp } from "@/helpers/common";
 import { User } from "@supabase/auth-js";
 import { theme } from "@/constants/theme";
+import Icon from "@/assets/icons";
+import { supabase } from "@/lib/supabase";
 
 interface IUserHeaderProps {
   user: User | null;
   router: Router;
+  handleLogout: () => void;
 }
 
 const Profile = () => {
   const { user, setAuth } = useAuth();
   const router = useRouter();
 
+  const onLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      Alert.alert("Sign out", "Error signing out");
+    }
+  };
+
+  const handleLogout = async () => {
+    Alert.alert("Log out", "Are you sure you want to log out?", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Modal cancelled"),
+        style: "cancel",
+      },
+      {
+        text: "Logout",
+        onPress: onLogout,
+        style: "destructive",
+      },
+    ]);
+  };
+
   return (
     <ScreenWrapper bg={"white"}>
-      <UserHeader user={user} router={router} />
-      <Text>Profile</Text>
+      <UserHeader user={user} router={router} handleLogout={handleLogout} />
     </ScreenWrapper>
   );
 };
 
-const UserHeader = ({ user, router }: IUserHeaderProps) => {
-  const handleLogout = async () => {};
-
+const UserHeader = ({ user, router, handleLogout }: IUserHeaderProps) => {
   return (
     <View
       style={{ flex: 1, backgroundColor: "white", paddingHorizontal: wp(4) }}
     >
       <View>
         <Header title={"Profile"} showBackButton />
-        <TouchableOpacity
-          style={styles.logoutButton}
-          onPress={handleLogout}
-        ></TouchableOpacity>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Icon name={"logout"} color={"red"} />
+        </TouchableOpacity>
       </View>
     </View>
   );
