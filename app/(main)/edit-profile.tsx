8ba085e4a00a6a50ae97cd8bf69edc12830e0be6
latin1gor <latinigor.main@gmail.com>
2024-09-17 13:16:37 +0300

    edit profile styles

--- app/(main)/edit-profile.tsx
+++ app/(main)/edit-profile.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { hp, wp } from "@/helpers/common";
+import { theme } from "@/constants/theme";
 
 const EditProfile = () => {
   return (
@@ -9,3 +11,61 @@ const EditProfile = () => {
 };
 
 export default EditProfile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: wp(4),
+  },
+
+  avatarContainer: {
+    height: hp(14),
+    width: hp(14),
+    alignSelf: "center",
+  },
+
+  avatar: {
+    width: "100%",
+    height: "100%",
+    borderRadius: theme.radius.xxl * 1.8,
+    borderCurve: "continuous",
+    borderWidth: 1,
+    borderColor: theme.colors.darkLight,
+  },
+
+  cameraIcon: {
+    position: "absolute",
+    bottom: 0,
+    right: -10,
+    padding: 0,
+    borderRadius: 50,
+    backgroundColor: "white",
+    shadowColor: theme.colors.textLight,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.4,
+    shadowRadius: 5,
+    elevation: 7,
+  },
+
+  form: {
+    gap: 18,
+    marginTop: 20,
+  },
+
+  input: {
+    flexDirection: "row",
+    borderWidth: 0.4,
+    borderColor: theme.colors.text,
+    borderRadius: theme.radius.xxl,
+    borderCurve: "continuous",
+    padding: 17,
+    paddingHorizontal: 20,
+    gap: 15,
+  },
+  bio: {
+    flexDirection: "row",
+    height: hp(15),
+    alignItems: "flex-start",
+    paddingVertical: 15,
+  },
+});
