e71ae55a3d955c7a0d3bfe1b2ba2eb5a95ff5985
latin1gor <latinigor.main@gmail.com>
2024-09-19 22:45:18 +0300

    get file path func

--- services/imageService.ts
+++ services/imageService.ts
@@ -5,3 +5,20 @@ export const getUserImageSrc = (imagePath: string | undefined) => {
     return require("../assets/images/user.jpg");
   }
 };
+
+export const uploadFile = async (
+  folderName: string,
+  fileUri: string,
+  isImage: boolean = true,
+) => {
+  try {
+    const fileName = getFilePath(folderName, isImage);
+  } catch (e) {
+    console.log("File upload error: ", e);
+    return { success: false, msg: "Could not upload media" };
+  }
+};
+
+const getFilePath = (folderName: string, isImage: boolean) => {
+  return `/${folderName}/${new Date().getTime()}${isImage ? '.png'}}`;
+};
