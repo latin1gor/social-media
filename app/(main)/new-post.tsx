147b2533e577527ab642521a55fe84f688f5b489
latin1gor <latinigor.main@gmail.com>
2024-09-20 11:03:33 +0300

    added styles

--- app/(main)/new-post.tsx
+++ app/(main)/new-post.tsx
@@ -1,6 +1,8 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ScreenWrapper from "@/components/screen-wrapper";
 import Header from "@/components/Header";
+import { hp, wp } from "@/helpers/common";
+import { theme } from "@/constants/theme";
 
 const NewPost = () => {
   return (
@@ -11,3 +13,64 @@ const NewPost = () => {
 };
 
 export default NewPost;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "red",
+    marginBottom: 30,
+    paddingHorizontal: wp(4),
+    gap: 15,
+  },
+  title: {
+    fontSize: hp(2.5),
+    fontWeight: "bold",
+    color: theme.colors.text,
+    textAlign: "center",
+  },
+  username: {
+    fontSize: hp(2.5),
+    fontWeight: "bold",
+    color: theme.colors.text,
+  },
+  avatar: {
+    height: hp(6.5),
+    width: hp(6.5),
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.1)",
+  },
+  publicText: {
+    fontSize: hp(1.7),
+    color: theme.colors.textLight,
+  },
+  textEditor: {
+    // marginTop: 10,
+  },
+  media: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1.5,
+    padding: 12,
+    paddingHorizontal: 18,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+    borderColor: theme.colors.gray,
+  },
+  mediaIcons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+  addImageText: {
+    fontSize: hp(1.9),
+    color: theme.colors.text,
+  },
+  imageIcon: {
+    // backgroundColor: theme.colors.gray,
+    borderRadius: theme.radius.md,
+    // padding: 6,
+  },
+});
