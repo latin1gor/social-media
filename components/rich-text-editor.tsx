239679f0f4153ab01a8ad92f541047e32f43ae4d
latin1gor <latinigor.main@gmail.com>
2024-09-23 22:35:08 +0300

    rich text editor feat

--- components/rich-text-editor.tsx
+++ components/rich-text-editor.tsx
@@ -1,12 +1,44 @@
 import React, { Ref } from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { actions, RichToolbar } from "react-native-pell-rich-editor";
 
 interface IRichTextEditorProps {
   editorRef?: Ref<any>;
-  onChange?: () => void;
+  onChange?: (body: any) => void;
 }
 const RichTextEditor = ({ editorRef, onChange }: IRichTextEditorProps) => {
-  return <Text>RichTextEditor</Text>;
+  return (
+    <View style={{ minHeight: 285 }}>
+      <RichToolbar
+        editor={editorRef || null}
+        actions={[
+          actions.setStrikethrough,
+          actions.removeFormat,
+          actions.setBold,
+          actions.setItalic,
+          actions.insertOrderedList,
+          actions.blockquote,
+          actions.alignLeft,
+          actions.alignRight,
+          actions.alignCenter,
+          actions.code,
+          actions.line,
+        ]}
+        disabled={false}
+        style={styles.richBar}
+        flatContainerStyle={styles.listStyle}
+      />
+    </View>
+  );
 };
 
 export default RichTextEditor;
+
+const styles = StyleSheet.create({
+  richBar: {
+    // Add any styles for the toolbar
+  },
+  listStyle: {
+    // Add any styles for the toolbar container
+  },
+});
